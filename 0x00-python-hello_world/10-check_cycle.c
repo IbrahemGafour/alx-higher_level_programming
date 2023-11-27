23be72da0affccd27f6e62d9457ff5b16c6ea35d
IbrahemGafour <ibrahem.abdelfatah.g4@gmail.com>
2023-11-27 12:12:54 -0800

    perfcto

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check lists if cycle
+ * @list - pointer to check list
+ * Return : 1 if cyclical, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+	listint_t *slow = list, *fast = list;
+	while (fast && fast->next)
+{
+		slow = slow->next;
+		fast = fast->next->next;
+		if(slow == fast)
+	return (1);
+}
+return(0);
