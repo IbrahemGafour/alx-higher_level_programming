efacc329683847aed02cb0a4259d387b102491d8
IbrahemGafour <IbrahemGafour>
2024-03-27 12:32:11 +0200

    test 21

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  return ((!num || num === 1) ? 1 : num * factorial(num - 1));
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
